f1b4b932088b4ec063847d5344d21ad781fffe65
ysirman <24975537+ysirman@users.noreply.github.com>
2020-06-12 23:29:15 +0900

    [add] Navigation

---
+++ front/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Navigation from "./Navigation";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Navigation />
+    </>
+  );
+}
+
+export default App;
