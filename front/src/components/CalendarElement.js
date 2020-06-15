b3a7ddf55a477901c823986214cb4129dc85f9ae
ysirman <24975537+ysirman@users.noreply.github.com>
2020-06-15 10:22:44 +0900

    [add] Schedule

--- front/src/components/CalendarElement.js
+++ front/src/components/CalendarElement.js
@@ -1,24 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
 import Box from "@material-ui/core/Box";
+import AppContext from "../contexts/AppContext";
+import Schedule from "./Schedule";
 
 function CalendarElement({ date, calendarLines }) {
   const ONE_WEEK_DAYS = 7;
+
+  const { state } = useContext(AppContext);
 
   // 日付（時間を除く）を比較して今日のスタイルを変える
   const today = new Date().toLocaleDateString(); // 2020/06/15
   let classOfDate = date.toLocaleDateString() === today ? "today" : "";
 
+  const schedules = state.schedule.filter(
+    (schedule) => schedule.date === date.toLocaleDateString()
+  );
   return (
     <>
       <Box
         className="calendar-item"
         width={1 / ONE_WEEK_DAYS}
         height={1 / calendarLines}
-        p={1}
-        pt={0.5}
+        py={0.5}
       >
         <Box align="center">
           <span className={classOfDate}>{date.getDate()}</span>
+        </Box>
+        <Box className="schedule-list" height="calc(100% - 24px)">
+          {schedules.map((schedule, index) => (
+            <Schedule key={index} schedule={schedule} />
+          ))}
         </Box>
       </Box>
     </>
